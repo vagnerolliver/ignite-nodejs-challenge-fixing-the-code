06f909212a1d62d4f54b6d933567d6e9b9d34cfd
Vagner Oliveira <vagner.olliver@yahoo.com.br>
2022-08-26 20:38:45 -0300

    chore: challenge done

--- src/index.js
+++ src/index.js
@@ -23,21 +23,24 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+  
   return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { likes, ...rest } = request.body
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
+  const updatedRepository = rest;
+ 
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+ 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
   repositories[repositoryIndex] = repository;
 
   return response.json(repository);
@@ -48,7 +51,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -67,8 +70,7 @@ app.post("/repositories/:id/like", (request, response) => {
   }
 
   const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
